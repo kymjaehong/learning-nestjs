a4aa19817f53da72d18cad11116c97fb0927dd7a
김재홍 <hyeop@gimjaehong-ui-MacBookAir.local>
2023-08-03 15:24:43 +0900

    add: divide module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
+// NestJS는 여러 개의 모듈로 이루어져 있다.
+// App Module은 App Controller, AppService만을 가져야 한다.
+// 대신 imports에 다른 모듈을 추가한다.
 @Module({
-  imports: [],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
