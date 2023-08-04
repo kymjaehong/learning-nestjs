0954f4516a847ba2b87e83dccc3ed117964d4282
김재홍 <hyeop@gimjaehong-ui-MacBookAir.local>
2023-08-05 00:53:05 +0900

    add: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach -> All
+  // 테스트가 실행될 때마다 새롭게 애플리케이션을 실행하는 것을 방지
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // json에 불필요한 프로퍼티 무시
+        forbidNonWhitelisted: true, // json에 불필요한 프로퍼티 존재 시, 에러 발생
+        transform: true, // json에서 보낸 프로퍼티 타입을 원하는 타입으로 변경 (url parameter's default type: string)
+      })
+    )
     await app.init();
   });
 
@@ -30,7 +39,7 @@ describe('AppController (e2e)', () => {
         .expect([])
     })
 
-    it("POST", () => {
+    it("POST 201", () => {
       return request(app.getHttpServer())
         .post("/movies")
         .send({
@@ -41,12 +50,45 @@ describe('AppController (e2e)', () => {
         .expect(201)
     })
 
+    it("POST 400", () => {
+      return request(app.getHttpServer())
+        .post("/movies")
+        .send({
+          title: "test",
+          year: 2023,
+          genres: ["test"],
+          other: "thing"
+        })
+        .expect(400)
+    })
+
     it("DELETE", () => {
       return request(app.getHttpServer())
         .delete("/movies")
         .expect(404)
     })
   })
+
+  describe("/movies/:id", () => {
+    
+    it("GET 200", () => {
+      // test 서버에서는 id가 string으로 나타난다.
+      // 왜 transform이 동작하지 않는가?
+      // main.ts에 설정한 app 설정을 동일하게 추가
+      return request(app.getHttpServer()).get("/movies/1").expect(200)
+    })
+
+    it("GET 404", () => {
+      return request(app.getHttpServer()).get("/movies/999").expect(404)
+    })
+    it("PATCH 200", () => {
+      return request(app.getHttpServer()).patch("/movies/1").send({title: "updated test"}).expect(200)
+    })
+    it("DELETE 200", () => {
+      return request(app.getHttpServer()).delete("/movies/1").expect(200)
+    })
+  })
+
   
 
 });
